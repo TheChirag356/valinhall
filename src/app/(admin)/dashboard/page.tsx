1f88cd8dc78190295dd5f69662df8398acaeaeab
TheChirag356 <arorachirag12005@gmail.com>
2025-04-17 00:52:20 +0530

    feat: add user settings pages and forms for profile, notifications, and display settings
    - Created `SettingsDisplayPage` for managing display settings.
    - Implemented `SettingsLayout` to structure user settings pages.
    - Developed `NotificationsForm` for configuring notification preferences.
    - Added `SettingsNotificationsPage` to handle notifications settings.
    - Created `SettingsProfilePage` for user profile management.
    - Implemented `ProfileForm` for updating user profile information.
    - Introduced reusable UI components including `Calendar`, `Command`, `Dialog`, `Form`, `Popover`, `RadioGroup`, `Switch`, and `Textarea`.

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,9 +1,158 @@
-export default function Projects() {
-    return (
-        <div className="flex justify-center items-center flex-col relative h-dvh bg-[#e8e8e8] dark:bg-[#181818]">
-            <div className="absolute top-36 left-10">
+"use client";
 
-            </div>
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Separator } from "@/components/ui/separator";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+} from "@/components/ui/dialog";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const frameworks = [
+  {
+    value: "he23ljio23oi1", // projectId
+    label: "HLNA",
+  },
+];
+
+export default function Projects() {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+  const [newProjectName, setNewProjectName] = React.useState("Project Name");
+  const [newProjectDescription, setNewProjectDescription] = React.useState(
+    "Project Description"
+  );
+
+  return (
+    <div className="flex justify-center items-center flex-col relative h-dvh bg-[#e8e8e8] dark:bg-[#181818]">
+      <div className="min-h-56 w-1/3 bg-black rounded-2xl flex flex-col justify-center items-center gap-4 p-6 border-1 border-[#262626]">
+        <h1 className="font-[family-name:var(--font-belanosima)] dark:text-white">
+          Choose an existing project
+        </h1>
+        <div>
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Button
+                variant="outline"
+                role="combobox"
+                aria-expanded={open}
+                className="w-[200px] justify-between"
+              >
+                {value
+                  ? frameworks.find((framework) => framework.value === value)
+                      ?.label
+                  : "Select your project..."}
+                <ChevronsUpDown className="opacity-50" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+              <Command>
+                <CommandInput
+                  placeholder="Search your projects..."
+                  className="h-9"
+                />
+                <CommandList>
+                  <CommandEmpty>No Project found.</CommandEmpty>
+                  <CommandGroup>
+                    {frameworks.map((framework) => (
+                      <CommandItem
+                        key={framework.value}
+                        value={framework.value}
+                        onSelect={(currentValue) => {
+                          setValue(currentValue === value ? "" : currentValue);
+                          setOpen(false);
+                        }}
+                      >
+                        {framework.label}
+                        <Check
+                          className={cn(
+                            "ml-auto",
+                            value === framework.value
+                              ? "opacity-100"
+                              : "opacity-0"
+                          )}
+                        />
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
         </div>
-    )
+
+        <Separator />
+        <h1 className="font-[family-name:var(--font-belanosima)] dark:text-white">
+          Or create a new one
+        </h1>
+        <div>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline">Create</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Create New Project</DialogTitle>
+                <DialogDescription>
+                  Create a new project to start using Valinhall.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="name" className="text-right">
+                    Project Name
+                  </Label>
+                  <Input
+                    id="name"
+                    value={newProjectName}
+                    className="col-span-3"
+                    onChange={(e) => setNewProjectName(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Project Description
+                </Label>
+                <Input
+                  id="description"
+                  value={newProjectDescription}
+                  className="col-span-3"
+                  onChange={(e) => setNewProjectDescription(e.target.value)}
+                />
+              </div>
+              <DialogFooter>
+                <Button type="submit">Let&apos;s Go</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
+    </div>
+  );
 }
