a168547556d0aa03398388be57e15035030bbadc
TheChirag356 <arorachirag12005@gmail.com>
2026-03-24 12:05:53 +0530

    updating vite configs

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -33,5 +33,8 @@ export default defineConfig({
 				}
 			}
 		]
+	},
+	"server": {
+		"port": 5173
 	}
 });
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -32,5 +32,8 @@ export default defineConfig({
 				}
 			}
 		]
+	},
+	"server": {
+		"port": 3000
 	}
 });
